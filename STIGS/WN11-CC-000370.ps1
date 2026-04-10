194f0c6d8e74460c1a208e7a62f328948aadf779
Uchenna Nicole <uchennanicolen@gmail.com>
2026-04-10 11:05:56 -0400

    Update WN11-CC-000370.ps1

--- STIGS/WN11-CC-000370.ps1
+++ STIGS/WN11-CC-000370.ps1
@@ -1,1 +1,26 @@
+ <#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
 
+.NOTES
+    Author          : Uchenna Nwankwo
+    LinkedIn        : linkedin.com/in/uchennanwankwo/
+    GitHub          : github.com/uchennanicole
+    Date Created    : 2026-10-04
+    Last Modified   : 2026-10-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000370
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN11-CC-000370.ps1 
+#>
