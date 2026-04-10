b3986170936b8d6e363a14da7d9d8241ab32d5a1
Uchenna Nicole <uchennanicolen@gmail.com>
2026-04-10 11:12:54 -0400

    Update WN11-CC-000325.ps1

--- STIGS/WN11-CC-000325.ps1
+++ STIGS/WN11-CC-000325.ps1
@@ -1,1 +1,27 @@
+ <#
+.SYNOPSIS
+  STIG WN11-CC-000038 — Disable WDigest Authentication (UseLogonCredential)
+
+.NOTES
+    Author          : Uchenna Nwankwo
+    LinkedIn        : linkedin.com/in/uchennanwankwo/
+    GitHub          : github.com/uchennanicole
+    Date Created    : 2024-10-04
+    Last Modified   : 2024-10-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000325
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN11-CC-000325..ps1 
+#><#
 
