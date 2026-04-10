16cc3f2e7b038dcd2faa04926069061131279992
Uchenna Nicole <uchennanicolen@gmail.com>
2026-04-10 02:17:55 -0400

    Update WN11-CC-000185.ps1

--- STIGS/WN11-CC-000185.ps1
+++ STIGS/WN11-CC-000185.ps1
@@ -24,6 +24,6 @@
     Example syntax:
     PS C:\> .\STIG-ID-WN11-CC-000185.ps1 
     
-#>
+#><#
 
 <#Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoAutorun" -Value 1 -Type DWord -Force
