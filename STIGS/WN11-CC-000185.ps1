44c62b7efe8f45ad3f0697bea16c6e538b242dfc
Uchenna Nicole <uchennanicolen@gmail.com>
2026-04-10 02:19:01 -0400

    Update WN11-CC-000185.ps1

--- STIGS/WN11-CC-000185.ps1
+++ STIGS/WN11-CC-000185.ps1
@@ -26,4 +26,6 @@
     
 #><#
 
+#Requires -RunAsAdministrator
+
 <#Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoAutorun" -Value 1 -Type DWord -Force
